2ce795e40146332c72c5211bf9e23e663b11c620
scy8848 <15170978735@163.com>
2026-09-07 11:14:27 +0800

    fix railway listen host

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,17 @@
-import { app } from './app.js'
-import { env } from './config/env.js'
-import { prisma } from './lib/prisma.js'
+import { app } from "./app.js";
+import { env } from "./config/env.js";
+import { prisma } from "./lib/prisma.js";
 
-const server = app.listen(env.port, () => console.log(`MORI API running at http://localhost:${env.port}`))
+const server = app.listen(env.port, "0.0.0.0", () => {
+  console.log(`MORI API running at http://0.0.0.0:${env.port}`);
+});
 
 async function shutdown() {
-  server.closeAllConnections()
-  server.close(async () => { await prisma.$disconnect(); process.exit(0) })
+  server.closeAllConnections();
+  server.close(async () => {
+    await prisma.$disconnect();
+    process.exit(0);
+  });
 }
-process.on('SIGINT', shutdown)
-process.on('SIGTERM', shutdown)
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
